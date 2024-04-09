cf218d785e092955ff8d3614735911650a634997
Jokkilook <tjddbs8295@gmail.com>
2024-04-09 23:39:41 +0900

    채팅방 안에서 상대방 프로필 사진,이름,전송시간 출력, 프로필 사진 누르면 그 프로필로 이동 기능 추가

--- lib/screens/chatting/chat_room_screen.dart
+++ lib/screens/chatting/chat_room_screen.dart
@@ -2,6 +2,7 @@ import 'package:campusmate/models/chat_room_data.dart';
 import 'package:campusmate/models/message_data.dart';
 import 'package:campusmate/modules/database.dart';
 import 'package:campusmate/provider/user_data_provider.dart';
+import 'package:campusmate/screens/profile/stranger_profile_screen.dart';
 import 'package:campusmate/widgets/input_text_field.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
@@ -23,31 +24,31 @@ class ChatRoomScreen extends StatefulWidget {
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
   var chattings = [];
-  Map<String, String> nameMap = {};
+  Map<String, List<String>> nameMap = {};
   final FocusNode focusNode = FocusNode();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getNames();
+    getNamesAndImages();
   }
 
-  void getNames() async {
+  void getNamesAndImages() async {
     widget.chatRoomData.participantsUid!.forEach((element) async {
-      await widget.db.db
-          .collection("users")
-          .doc(element)
-          .get()
-          .then((value) => nameMap[element] = value["name"]);
+      await widget.db.db.collection("users").doc(element).get().then((value) =>
+          nameMap[element] = [
+            value["name"].toString(),
+            value["imageUrl"].toString()
+          ]);
     });
   }
 
-  void sendMessage() {
+  void sendMessage() async {
     if (widget.chatController.value.text == "") {
       return;
     }
-    widget.db.db
+    await widget.db.db
         .collection("chats/${widget.chatRoomData.roomId}/messages")
         .doc()
         .set(MessageData(
@@ -72,24 +73,44 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
           title: Text(widget.chatRoomData.roomName ?? ""),
         ),
         bottomNavigationBar: Container(
-          padding: const EdgeInsets.all(10),
           color: Colors.white,
           height: 70,
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Expanded(
-                  child: InputTextField(
-                focusNode: focusNode,
-                controller: widget.chatController,
-                hintText: "메세지를 입력하세요.",
-              )),
-              const SizedBox(width: 10),
-              IconButton(
+              //+버튼
+              FilledButton(
+                  style: FilledButton.styleFrom(
+                      fixedSize: const Size(60, 60),
+                      backgroundColor: Colors.green,
+                      shape: const ContinuousRectangleBorder()),
                   onPressed: () {
                     focusNode.requestFocus();
                     sendMessage();
                   },
-                  icon: const Icon(Icons.send))
+                  child: const Icon(Icons.add)),
+              //텍스트 입력창
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: InputTextField(
+                  focusNode: focusNode,
+                  controller: widget.chatController,
+                  hintText: "메세지를 입력하세요.",
+                ),
+              )),
+              //보내기 버튼
+              FilledButton(
+                  style: FilledButton.styleFrom(
+                      fixedSize: const Size(60, 60),
+                      backgroundColor: Colors.green,
+                      shape: const ContinuousRectangleBorder()),
+                  onPressed: () {
+                    focusNode.requestFocus();
+                    sendMessage();
+                  },
+                  child: const Icon(Icons.send)),
             ],
           ),
         ),
@@ -111,10 +132,10 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
 
                 if (snapshot.hasData) {
                   return Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    color: Colors.white,
+                    color: Colors.grey[200],
                     height: double.infinity,
                     child: ListView.separated(
+                      padding: const EdgeInsets.all(10),
                       separatorBuilder: (context, index) =>
                           const SizedBox(height: 10),
                       reverse: true,
@@ -146,28 +167,39 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                               snapshot.data!.docs[index + 1]["senderUID"]) {
                             return OtherChatUnit(
                                 data: snapshot.data!.docs[index],
+                                senderUid: snapshot.data!.docs[index]
+                                    ["senderUID"],
                                 viewSender: true,
                                 name: nameMap[snapshot.data!.docs[index]
-                                        ["senderUID"]] ??
+                                        ["senderUID"]]![0] ??
                                     "안불러와졍",
                                 index: index,
-                                showTime: showTime);
+                                showTime: showTime,
+                                imageUrl: nameMap[snapshot.data!.docs[index]
+                                    ["senderUID"]]![1]);
                           }
                         } catch (e) {
                           return OtherChatUnit(
                               data: snapshot.data!.docs[index],
+                              senderUid: snapshot.data!.docs[index]
+                                  ["senderUID"],
                               viewSender: true,
                               name: nameMap[snapshot.data!.docs[index]
-                                      ["senderUID"]] ??
+                                      ["senderUID"]]![0] ??
                                   "안불러와졍",
                               index: index,
-                              showTime: showTime);
+                              showTime: showTime,
+                              imageUrl: nameMap[snapshot.data!.docs[index]
+                                  ["senderUID"]]![1]);
                         }
 
                         return OtherChatUnit(
                             data: snapshot.data!.docs[index],
+                            senderUid: snapshot.data!.docs[index]["senderUID"],
                             index: index,
-                            showTime: showTime);
+                            showTime: showTime,
+                            imageUrl: nameMap[snapshot.data!.docs[index]
+                                ["senderUID"]]![1]);
                       },
                     ),
                   );
@@ -198,12 +230,15 @@ class MyChatUnit extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          showTime ? Text(timeStampToHourMinutes(data["time"])) : Container(),
+          showTime
+              ? Text(timeStampToHourMinutes(data["time"]),
+                  style: const TextStyle(fontSize: 10))
+              : Container(),
           const SizedBox(width: 6),
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.7),
+                maxWidth: MediaQuery.of(context).size.width * 0.6),
             decoration: BoxDecoration(
                 color: Colors.green, borderRadius: BorderRadius.circular(10)),
             child: Column(
@@ -227,16 +262,20 @@ class OtherChatUnit extends StatelessWidget {
   const OtherChatUnit(
       {super.key,
       required this.data,
+      required this.senderUid,
       this.viewSender = false,
       this.name = "알 수 없음",
       this.index = 0,
-      this.showTime = false});
+      this.showTime = false,
+      this.imageUrl = ""});
 
   final QueryDocumentSnapshot data;
   final bool viewSender;
+  final String senderUid;
   final String name;
   final int index;
   final bool showTime;
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -250,7 +289,25 @@ class OtherChatUnit extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               viewSender
-                  ? const CircleAvatar(radius: 25)
+                  ? GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                StrangerProfilScreen(uid: senderUid),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10)),
+                        width: 50,
+                        height: 50,
+                        child: Image.network(imageUrl, fit: BoxFit.cover),
+                      ),
+                    )
                   : Container(
                       width: 50,
                     ),
@@ -264,7 +321,7 @@ class OtherChatUnit extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(
                         horizontal: 15, vertical: 10),
                     constraints: BoxConstraints(
-                        maxWidth: MediaQuery.of(context).size.width * 0.8),
+                        maxWidth: MediaQuery.of(context).size.width * 0.6),
                     decoration: BoxDecoration(
                         color: Colors.blue,
                         borderRadius: BorderRadius.circular(10)),
@@ -284,7 +341,10 @@ class OtherChatUnit extends StatelessWidget {
             ],
           ),
           const SizedBox(width: 6),
-          showTime ? Text(timeStampToHourMinutes(data["time"])) : Container(),
+          showTime
+              ? Text(timeStampToHourMinutes(data["time"]),
+                  style: const TextStyle(fontSize: 10))
+              : Container(),
         ],
       ),
     );
