8b5a1bd66f3e69af9f262f9331a4083b99c0306a
gwanwoo <ppkw2001@gmail.com>
2024-04-11 11:09:31 +0900

    일반게시판 리스트 크기 지정

--- lib/widgets/general_board_item.dart
+++ lib/widgets/general_board_item.dart
@@ -28,110 +28,113 @@ class GeneralBoardItem extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // 제목
-                    SizedBox(
-                      width: 250,
-                      child: Text(
-                        postData.title ?? '제목 없음',
-                        overflow:
-                            TextOverflow.ellipsis, // 텍스트가 영역을 벗어날 때 "..."으로 처리
-                        maxLines: 1, // 텍스트가 한 줄로만 표시되도록 제한
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                SizedBox(
+                  height: 110,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // 제목
+                      SizedBox(
+                        width: 250,
+                        child: Text(
+                          postData.title ?? '제목 없음',
+                          overflow: TextOverflow
+                              .ellipsis, // 텍스트가 영역을 벗어날 때 "..."으로 처리
+                          maxLines: 1, // 텍스트가 한 줄로만 표시되도록 제한
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    // 내용 첫줄
-                    SizedBox(
-                      width: 250,
-                      child: Text(
-                        postData.content ?? '내용 없음',
-                        overflow:
-                            TextOverflow.ellipsis, // 텍스트가 영역을 벗어날 때 "..."으로 처리
-                        maxLines: 1, // 텍스트가 한 줄로만 표시되도록 제한
+                      // 내용 첫줄
+                      SizedBox(
+                        width: 250,
+                        child: Text(
+                          postData.content ?? '내용 없음',
+                          overflow: TextOverflow
+                              .ellipsis, // 텍스트가 영역을 벗어날 때 "..."으로 처리
+                          maxLines: 1, // 텍스트가 한 줄로만 표시되도록 제한
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        // 작성자 닉네임
-                        Text(
-                          postData.author ?? 'null',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          // 작성자 닉네임
+                          Text(
+                            postData.author ?? 'null',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
                           ),
-                        ),
-                        const SizedBox(width: 6),
-                        // 작성 시간
-                        Text(
-                          '| $formattedTime',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
+                          const SizedBox(width: 6),
+                          // 작성 시간
+                          Text(
+                            '| $formattedTime',
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 2,
-                    ),
-                    Row(
-                      children: [
-                        // 좋아요
-                        const Icon(
-                          Icons.thumb_up_alt_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.likeCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      Row(
+                        children: [
+                          // 좋아요
+                          const Icon(
+                            Icons.thumb_up_alt_outlined,
                             color: Colors.grey,
+                            size: 16,
                           ),
-                        ),
-                        const SizedBox(width: 10),
-                        // 싫어요
-                        const Icon(
-                          Icons.thumb_down_alt_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.dislikeCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
+                          const SizedBox(width: 4),
+                          Text(
+                            postData.likeCount.toString(),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          // 싫어요
+                          const Icon(
+                            Icons.thumb_down_alt_outlined,
                             color: Colors.grey,
+                            size: 16,
                           ),
-                        ),
-                        const SizedBox(width: 10),
-                        // 댓글
-                        const Icon(
-                          Icons.mode_comment_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.commentCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
+                          const SizedBox(width: 4),
+                          Text(
+                            postData.dislikeCount.toString(),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          // 댓글
+                          const Icon(
+                            Icons.mode_comment_outlined,
                             color: Colors.grey,
+                            size: 16,
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          const SizedBox(width: 4),
+                          Text(
+                            postData.commentCount.toString(),
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 Container(
                   width: 80,
