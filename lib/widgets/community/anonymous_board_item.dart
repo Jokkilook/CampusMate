c78051387996d696efd02501faa94f7331a63cdd
Jokkilook <tjddbs8295@gmail.com>
2024-04-20 23:29:39 +0900

    게시판 아이템 코드 간결화, 디자인 수

--- lib/widgets/community/anonymous_board_item.dart
+++ lib/widgets/community/anonymous_board_item.dart
@@ -19,134 +19,134 @@ class AnonymousBoardItem extends StatelessWidget {
     String formattedTime = formatTimeStamp(postData.timestamp!, now);
 
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       width: double.infinity,
-      height: 130,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 110,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      children: [
-                        // 제목
-                        SizedBox(
-                          width: 250,
-                          child: Text(
-                            postData.title ?? '제목 없음',
-                            overflow: TextOverflow.ellipsis, // ...처리
-                            maxLines: 1, // 1줄 제한
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        // 내용 첫줄
-                        SizedBox(
-                          width: 250,
-                          child: Text(
-                            postData.content ?? '내용 없음',
-                            overflow: TextOverflow.ellipsis, // ...처리
-                            maxLines: 2, // 2줄 제한
-                          ),
-                        ),
-                      ],
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 제목
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Text(
+                    postData.title ?? '제목 없음',
+                    overflow: TextOverflow.ellipsis, // ...처리
+                    maxLines: 1, // 1줄 제한
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Row(
-                      children: [
-                        // 좋아요
-                        const Icon(
-                          Icons.thumb_up_alt_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.likeCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        // 싫어요
-                        const Icon(
-                          Icons.thumb_down_alt_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.dislikeCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        // 댓글
-                        const Icon(
-                          Icons.mode_comment_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.commentCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        // 조회수
-                        const Icon(
-                          Icons.account_circle_outlined,
-                          color: Colors.grey,
-                          size: 16,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          postData.viewCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        const SizedBox(width: 6),
-                        // 작성 시간
-                        Text(
-                          '| $formattedTime',
-                          style: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
+                  ),
+                ),
+                // 내용 첫줄
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Text(
+                    textAlign: TextAlign.start,
+                    postData.content ?? '내용 없음',
+                    maxLines: 2, // 2줄 제한
+                    style: const TextStyle(
+                      textBaseline: TextBaseline.alphabetic,
+                      overflow: TextOverflow.ellipsis, //오버플로우 ... 처리
+                      height: 3,
+                    ),
+                    textHeightBehavior: const TextHeightBehavior(
+                      applyHeightToFirstAscent: true,
+                      applyHeightToLastDescent: true,
+                      leadingDistribution: TextLeadingDistribution.even,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    // 좋아요
+                    const Icon(
+                      Icons.thumb_up_alt_outlined,
+                      color: Colors.grey,
+                      size: 14,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      postData.likeCount.toString(),
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    // 싫어요
+                    const Icon(
+                      Icons.thumb_down_alt_outlined,
+                      color: Colors.grey,
+                      size: 14,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      postData.dislikeCount.toString(),
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    // 댓글
+                    const Icon(
+                      Icons.mode_comment_outlined,
+                      color: Colors.grey,
+                      size: 14,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      postData.commentCount.toString(),
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    // 조회수
+                    const Icon(
+                      Icons.account_circle_outlined,
+                      color: Colors.grey,
+                      size: 14,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      postData.viewCount.toString(),
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(width: 6),
+                    // 작성 시간
+                    Text(
+                      '| $formattedTime',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Container(
-                width: 80,
-                height: 80,
+              ],
+            ),
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
                 decoration: BoxDecoration(
                   color: Colors.grey,
                   borderRadius: BorderRadius.circular(5),
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/community/general_board_item.dart
+++ lib/widgets/community/general_board_item.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../models/post_data.dart';
 import '../../modules/format_time_stamp.dart';
@@ -20,47 +22,52 @@ class GeneralBoardItem extends StatelessWidget {
     String formattedTime = formatTimeStamp(postData.timestamp!, now);
 
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       width: double.infinity,
-      height: 130,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 110,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //height: 100,
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //제목 / 내용
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     // 제목
                     SizedBox(
-                      width: 250,
+                      width: MediaQuery.of(context).size.width * 0.6,
                       child: Text(
                         postData.title ?? '제목 없음',
                         overflow: TextOverflow.ellipsis,
                         maxLines: 1,
                         style: const TextStyle(
-                          fontSize: 20,
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                     // 내용 첫줄
                     SizedBox(
-                      width: 250,
+                      width: MediaQuery.of(context).size.width * 0.6,
                       child: Text(
                         postData.content ?? '내용 없음',
                         overflow: TextOverflow.ellipsis,
                         maxLines: 1,
                       ),
                     ),
-                    const SizedBox(
-                      height: 5,
-                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 5),
+
+                //닉네임, 게시시간 / 좋,싫,댓,조
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //닉네임, 게시시간
                     Row(
                       children: [
                         // 작성자 닉네임 가져오기
@@ -75,17 +82,16 @@ class GeneralBoardItem extends StatelessWidget {
                             }
                             if (!snapshot.hasData || snapshot.data == null) {
                               return const Text(
-                                'name',
+                                '닉네임',
                                 style: TextStyle(
                                   fontSize: 14,
                                   color: Colors.grey,
                                 ),
                               );
                             }
-
                             // 문서에서 사용자 이름 가져오기
                             String authorName = snapshot.data!['name'];
-
+                            //작성자, 게시시간
                             return Row(
                               children: [
                                 Text(
@@ -110,22 +116,20 @@ class GeneralBoardItem extends StatelessWidget {
                         ),
                       ],
                     ),
-                    const SizedBox(
-                      height: 2,
-                    ),
+                    //좋아요, 싫어요, 댓글, 조회수
                     Row(
                       children: [
                         // 좋아요
                         const Icon(
                           Icons.thumb_up_alt_outlined,
                           color: Colors.grey,
-                          size: 16,
+                          size: 14,
                         ),
                         const SizedBox(width: 4),
                         Text(
                           postData.likeCount.toString(),
                           style: const TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.grey,
                           ),
                         ),
@@ -134,13 +138,13 @@ class GeneralBoardItem extends StatelessWidget {
                         const Icon(
                           Icons.thumb_down_alt_outlined,
                           color: Colors.grey,
-                          size: 16,
+                          size: 14,
                         ),
                         const SizedBox(width: 4),
                         Text(
                           postData.dislikeCount.toString(),
                           style: const TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.grey,
                           ),
                         ),
@@ -149,13 +153,13 @@ class GeneralBoardItem extends StatelessWidget {
                         const Icon(
                           Icons.mode_comment_outlined,
                           color: Colors.grey,
-                          size: 16,
+                          size: 14,
                         ),
                         const SizedBox(width: 4),
                         Text(
                           postData.commentCount.toString(),
                           style: const TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.grey,
                           ),
                         ),
@@ -164,13 +168,13 @@ class GeneralBoardItem extends StatelessWidget {
                         const Icon(
                           Icons.account_circle_outlined,
                           color: Colors.grey,
-                          size: 16,
+                          size: 14,
                         ),
                         const SizedBox(width: 4),
                         Text(
                           postData.viewCount.toString(),
                           style: const TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Colors.grey,
                           ),
                         ),
@@ -178,18 +182,20 @@ class GeneralBoardItem extends StatelessWidget {
                     ),
                   ],
                 ),
-              ),
-              Container(
-                width: 80,
-                height: 80,
+              ],
+            ),
+            //이미지 썸네일
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
                 decoration: BoxDecoration(
                   color: Colors.grey,
                   borderRadius: BorderRadius.circular(5),
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
