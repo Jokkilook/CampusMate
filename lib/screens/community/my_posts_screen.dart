fd73daddfe3a40f79114aae5f365dae68f576de9
gwanwoo <ppkw2001@gmail.com>
2024-05-27 14:28:18 +0900

    버그 수정

--- lib/screens/community/my_posts_screen.dart
+++ lib/screens/community/my_posts_screen.dart
@@ -1,3 +1,4 @@
+import 'package:campusmate/app_colors.dart';
 import 'package:campusmate/models/user_data.dart';
 import 'package:campusmate/provider/user_data_provider.dart';
 import 'package:campusmate/screens/community/models/post_data.dart';
@@ -6,13 +7,11 @@ import 'package:flutter/material.dart';
 import 'package:campusmate/screens/community/widgets/general_board_item.dart';
 import 'package:campusmate/screens/community/widgets/anonymous_board_item.dart';
 import 'package:provider/provider.dart';
+
 import 'post_screen.dart';
 
 class MyPostsScreen extends StatelessWidget {
-  const MyPostsScreen({super.key});
-
-  static const String generalPostsCollection = 'generalPosts';
-  static const String anonymousPostsCollection = 'anonymousPosts';
+  const MyPostsScreen({Key? key}) : super(key: key);
 
   Future<List<DocumentSnapshot>> _fetchUserPosts(BuildContext context) async {
     try {
@@ -22,29 +21,34 @@ class MyPostsScreen extends StatelessWidget {
 
       final firestore = FirebaseFirestore.instance;
 
+      // 사용자가 작성한 일반 게시물과 익명 게시물을 시간순으로 정렬하여 가져옴
       var generalPostsQuery = firestore
-          .collection("schools/$school/$generalPostsCollection")
+          .collection("schools/$school/generalPosts")
           .where('authorUid', isEqualTo: currentUserUid)
           .orderBy('timestamp', descending: true);
       var anonymousPostsQuery = firestore
-          .collection("schools/$school/$anonymousPostsCollection")
+          .collection("schools/$school/anonymousPosts")
           .where('authorUid', isEqualTo: currentUserUid)
           .orderBy('timestamp', descending: true);
 
+      // 쿼리 실행
       List<QuerySnapshot> querySnapshots = await Future.wait([
         generalPostsQuery.get(),
         anonymousPostsQuery.get(),
       ]);
 
+      // 결과 병합
       List<DocumentSnapshot> allResults = [
         ...querySnapshots[0].docs,
         ...querySnapshots[1].docs,
       ];
 
-      // 디버깅 로그 출력
-      for (var doc in allResults) {
-        debugPrint('Fetched post: ${doc.data()}');
-      }
+      // 시간순으로 정렬
+      allResults.sort((a, b) {
+        Timestamp timestampA = a['timestamp'] as Timestamp;
+        Timestamp timestampB = b['timestamp'] as Timestamp;
+        return timestampB.compareTo(timestampA);
+      });
 
       return allResults;
     } catch (e) {
@@ -67,7 +71,7 @@ class MyPostsScreen extends StatelessWidget {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return const Center(child: Text('오류가 발생했습니다. 나중에 다시 시도해주세요.'));
+            return const Center(child: Text('오류가 발생했습니다.'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return const Center(child: Text('작성한 글이 없습니다.'));
           }
@@ -79,8 +83,47 @@ class MyPostsScreen extends StatelessWidget {
             itemBuilder: (context, index) {
               var postData = PostData.fromJson(
                   userPosts[index].data() as Map<String, dynamic>);
-              return _buildPostItem(context, postData,
-                  userPosts[index].reference.parent.id, userData);
+              if (userPosts[index].reference.parent.id == 'generalPosts') {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PostScreen(
+                          postData: postData,
+                          firestore: FirebaseFirestore.instance,
+                          userData: userData,
+                        ),
+                      ),
+                    );
+                  },
+                  child: GeneralBoardItem(
+                    postData: postData,
+                    firestore: FirebaseFirestore.instance,
+                  ),
+                );
+              } else if (userPosts[index].reference.parent.id ==
+                  'anonymousPosts') {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PostScreen(
+                          postData: postData,
+                          firestore: FirebaseFirestore.instance,
+                          userData: userData,
+                        ),
+                      ),
+                    );
+                  },
+                  child: AnonymousBoardItem(
+                    postData: postData,
+                  ),
+                );
+              } else {
+                return Container();
+              }
             },
             separatorBuilder: (context, index) {
               return const Divider(
@@ -91,49 +134,5 @@ class MyPostsScreen extends StatelessWidget {
         },
       ),
     );
-  }
-
-  Widget _buildPostItem(BuildContext context, PostData postData,
-      String parentCollectionId, UserData userData) {
-    if (parentCollectionId == generalPostsCollection) {
-      return InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PostScreen(
-                postData: postData,
-                firestore: FirebaseFirestore.instance,
-                userData: userData,
-              ),
-            ),
-          );
-        },
-        child: GeneralBoardItem(
-          postData: postData,
-          firestore: FirebaseFirestore.instance,
-        ),
-      );
-    } else if (parentCollectionId == anonymousPostsCollection) {
-      return InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => PostScreen(
-                postData: postData,
-                firestore: FirebaseFirestore.instance,
-                userData: userData,
-              ),
-            ),
-          );
-        },
-        child: AnonymousBoardItem(
-          postData: postData,
-        ),
-      );
-    } else {
-      return Container();
-    }
   }
 }
